6a5eb914bd86728073691d90178a4f5fd9193067
Evgeny Ivanov <jevgenijs240693@gmail.com>
2020-05-17 15:02:18 +0100

    dobavil komandi

--- learn_git.py
+++ learn_git.py
@@ -1,7 +1,11 @@
-git init - zapusk git
-git add . -dobavitj izmeneija
-git add .A -dobavitj vse izmenenija
-git commit -m "coment" - sohanitj izmenieja+komentarij
-git status -posmotretj izmenenija
+git init                                -zapusk git
+git add .                               -dobavitj izmeneija
+git add .A                              -dobavitj vse izmenenija
+git commit -m "coment"                  -sohanitj izmenieja+komentarij
+git status                              -posmotretj izmenenija
 git config --global user.name "username"
 git config --global user.email usermail@.com
+git chekout HEAD                        -perejti k poslednemu commitu(poslednej versii koda)
+git log                                 -posmotretj istoriju izmenenij
+"""mozno sozdatj papku s nazvaniem .gitignore dlja togo 4tobi git ih ne videl i ne trogal"""
+.gitignore
