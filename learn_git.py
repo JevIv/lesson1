ea61e8227887742779b50d310f5649b9307a0e4c
Evgeny Ivanov <jevgenijs240693@gmail.com>
2020-05-17 14:43:54 +0100

    Bazovie komandi po gitu

---
+++ learn_git.py
@@ -0,0 +1,7 @@
+git init - zapusk git
+git add . -dobavitj izmeneija
+git add .A -dobavitj vse izmenenija
+git commit -m "coment" - sohanitj izmenieja+komentarij
+git status -posmotretj izmenenija
+git config --global user.name "username"
+git config --global user.email usermail@.com
